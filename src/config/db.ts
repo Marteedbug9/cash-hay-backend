2f522091085cbce88e3c8ce1224babf626c392b1
Marteedbug9 <martyveson2@gmail.com>
2025-05-24 13:25:26 -0400

    Update db.ts

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,14 +1,14 @@
-// src/config/db.ts
 import { Pool } from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false, // important pour Render
-  },
+  user: process.env.DB_USER || 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  database: process.env.DB_NAME || 'cashhay',
+  password: process.env.DB_PASSWORD || '',
+  port: Number(process.env.DB_PORT) || 5432,
 });
 
 export default pool;
