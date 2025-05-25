928d8a8d3c5737d0283838c4aa788dd3c19581e0
Marteedbug9 <martyveson2@email.com>
2025-05-25 18:35:01 -0400

    Fix: full package.json corrected for deployment

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,3 @@
-/*src/server.ts
-import express from 'express';
-import authRoutes from './routes/authRoutes';
-
-const app = express();
-const PORT = 3000;
-
-app.use(express.json());
-app.use('/api/auth', authRoutes);
-
-app.listen(PORT, () => {
-  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
-});*/
-
-
 // src/server.ts
 import './config/db'; // ← Connexion à la DB
 import dotenv from 'dotenv';
