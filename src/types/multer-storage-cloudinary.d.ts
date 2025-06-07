e396caac348c30ad6beaba79fba142b9e965b253
Marteedbug9 <martyveson2@email.com>
2025-06-07 09:13:32 -0400

    fix: ajout des types pour multer

--- src/types/multer-storage-cloudinary.d.ts
+++ src/types/multer-storage-cloudinary.d.ts
@@ -1,6 +1,7 @@
 declare module 'multer-storage-cloudinary' {
   import { StorageEngine } from 'multer';
-  import { ConfigOptions, UploadApiOptions, UploadApiResponse } from 'cloudinary';
+  import { UploadApiOptions } from 'cloudinary';
+  import { Request } from 'express';
 
   interface CloudinaryStorageOptions {
     cloudinary: any;
@@ -9,5 +10,17 @@ declare module 'multer-storage-cloudinary' {
 
   export class CloudinaryStorage implements StorageEngine {
     constructor(options: CloudinaryStorageOptions);
+
+    _handleFile(
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error?: any, info?: Partial<Express.Multer.File>) => void
+    ): void;
+
+    _removeFile(
+      req: Request,
+      file: Express.Multer.File,
+      callback: (error: Error) => void
+    ): void;
   }
 }
